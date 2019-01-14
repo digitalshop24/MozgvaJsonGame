3b30876dbe867b916fad4f9d6bfceed0514ab8ba
Sergey Ryabushko <biginf.com@gmail.com>
2019-01-14 15:36:55 +0300

    Initial Commit

---
+++ MozgvaJsonGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MozgvaJsonGame
+//
+//  Created by Sergey Ryabushko on 14/01/2019.
+//  Copyright © 2019 Sergey Ryabushko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
