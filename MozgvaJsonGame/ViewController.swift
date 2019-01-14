08bb119d8c094b62933296e3477ba6926ee022bb
Sergey Ryabushko <biginf.com@gmail.com>
2019-01-14 20:51:46 +0300

    take game name

--- MozgvaJsonGame/ViewController.swift
+++ MozgvaJsonGame/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
+struct Games : Decodable {
+    let id : Int
+    let GAME_NAME : String
+    let DATE : String
+    let TIME : String
+    let FREE : Bool
+    let PRICE : Int
+    let PLACE : String
+    let ADDRESS : String
+    let free_places_team : Int
+    let free_places_person : Int
+    let game_type : String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://mozgva.com/api/v1/games/schedule?id=1"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) {
+            (data, response, err) in
+
+            guard let data = data else { return }
+            
+            do {
+                let games = try
+                    JSONDecoder().decode([Games].self, from: data) //sozdaiom massiv iz igr
+                print(games.count) //vivodim massiv
+                let gameCount = games.count
+                var firstGame = 0
+
+                while firstGame <= gameCount - 1 {
+                    let GameName = games[firstGame]
+                    print(GameName.GAME_NAME)
+                    firstGame = firstGame + 1
+                }
+                
+                
+                
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+            
+            }.resume()
+        
+
+
     }
 
 
